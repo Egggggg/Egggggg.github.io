daa95058069cd8abde30223f7256c4c88b689cb7
Egggggg <mrhumanman2@gmail.com>
2019-01-12 00:59:48 -0500

    Here they come

--- old/sidebar.js
+++
@@ -1,9 +0,0 @@
-var active = "sbHome";
-
-function setActive(nowActive) {
-	if (active != nowActive) {
-		document.getElementById(active).className = "";
-		document.getElementById(nowActive).className = "active";
-		active = nowActive;
-	}
-}
